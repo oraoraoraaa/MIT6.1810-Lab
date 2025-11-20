f4f9474fa60db5b4d31fc4d37c5c7c819e70be52
Rinalic <rinalic39@gmail.com>
2025-11-20 10:59:07 +0900

    Auto formatting changes

--- xv6-OS/kernel/sysproc.c
+++ xv6-OS/kernel/sysproc.c
@@ -11,132 +11,136 @@
 #include "vm.h"
 
 uint64
-sys_exit(void)
+sys_exit (void)
 {
   int n;
-  argint(0, &n);
-  kexit(n);
-  return 0;  // not reached
+  argint (0, &n);
+  kexit (n);
+  return 0; // not reached
 }
 
 uint64
-sys_getpid(void)
+sys_getpid (void)
 {
-  return myproc()->pid;
+  return myproc ()->pid;
 }
 
 uint64
-sys_fork(void)
+sys_fork (void)
 {
-  return kfork();
+  return kfork ();
 }
 
 uint64
-sys_wait(void)
+sys_wait (void)
 {
   uint64 p;
-  argaddr(0, &p);
-  return kwait(p);
+  argaddr (0, &p);
+  return kwait (p);
 }
 
 uint64
-sys_sbrk(void)
+sys_sbrk (void)
 {
   uint64 addr;
   int t;
   int n;
 
-  argint(0, &n);
-  argint(1, &t);
-  addr = myproc()->sz;
+  argint (0, &n);
+  argint (1, &t);
+  addr = myproc ()->sz;
 
-  if(t == SBRK_EAGER || n < 0) {
-    if(growproc(n) < 0) {
-      return -1;
+  if (t == SBRK_EAGER || n < 0)
+    {
+      if (growproc (n) < 0)
+        {
+          return -1;
+        }
     }
-  } else {
-    // Lazily allocate memory for this process: increase its memory
-    // size but don't allocate memory. If the processes uses the
-    // memory, vmfault() will allocate it.
-    if(addr + n < addr)
-      return -1;
-    myproc()->sz += n;
-  }
+  else
+    {
+      // Lazily allocate memory for this process: increase its memory
+      // size but don't allocate memory. If the processes uses the
+      // memory, vmfault() will allocate it.
+      if (addr + n < addr)
+        return -1;
+      myproc ()->sz += n;
+    }
   return addr;
 }
 
 uint64
-sys_pause(void)
+sys_pause (void)
 {
   int n;
   uint ticks0;
 
-
-  argint(0, &n);
-  if(n < 0)
+  argint (0, &n);
+  if (n < 0)
     n = 0;
-  acquire(&tickslock);
+  acquire (&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(killed(myproc())){
-      release(&tickslock);
-      return -1;
+  while (ticks - ticks0 < n)
+    {
+      if (killed (myproc ()))
+        {
+          release (&tickslock);
+          return -1;
+        }
+      sleep (&ticks, &tickslock);
     }
-    sleep(&ticks, &tickslock);
-  }
-  release(&tickslock);
+  release (&tickslock);
   return 0;
 }
 
-
 #ifdef LAB_PGTBL
 int
-sys_pgpte(void)
+sys_pgpte (void)
 {
   uint64 va;
-  struct proc *p;  
+  struct proc *p;
 
-  p = myproc();
-  argaddr(0, &va);
-  pte_t *pte = pgpte(p->pagetable, va);
-  if(pte != 0) {
-      return (uint64) *pte;
-  }
+  p = myproc ();
+  argaddr (0, &va);
+  pte_t *pte = pgpte (p->pagetable, va);
+  if (pte != 0)
+    {
+      return (uint64)*pte;
+    }
   return 0;
 }
 #endif
 
 #ifdef LAB_PGTBL
 int
-sys_kpgtbl(void)
+sys_kpgtbl (void)
 {
-  struct proc *p;  
+  struct proc *p;
 
-  p = myproc();
-  vmprint(p->pagetable);
+  p = myproc ();
+  vmprint (p->pagetable);
   return 0;
 }
 #endif
 
-
 uint64
-sys_kill(void)
+sys_kill (void)
 {
   int pid;
 
-  argint(0, &pid);
-  return kkill(pid);
+  argint (0, &pid);
+  return kkill (pid);
 }
 
 // return how many clock tick interrupts have occurred
 // since start.
 uint64
-sys_uptime(void)
+sys_uptime (void)
 {
   uint xticks;
 
-  acquire(&tickslock);
+  acquire (&tickslock);
   xticks = ticks;
-  release(&tickslock);
+  release (&tickslock);
   return xticks;
 }
