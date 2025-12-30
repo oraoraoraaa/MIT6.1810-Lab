184b943be81a100e53ed386f94226da3386915e7
Rin_ArchLinux <rinalic39@gmail.com>
2025-12-30 19:08:20 +0900

    Complete RISC-V assembly

---
+++ xv6-OS/user/test.c
@@ -0,0 +1,11 @@
+#include "kernel/param.h"
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+void
+main (void)
+{
+  unsigned int i = 0x00646c72;
+  printf ("H%x Wo%s\n", 57616, (char *)&i);
+}
